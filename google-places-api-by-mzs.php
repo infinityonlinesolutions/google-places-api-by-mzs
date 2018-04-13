24cda56afd430e84f2f365fe69869150c0bbf0c0
Muhammad Zubair Saleem <mzubairsaleem@outlook.com>
2018-04-14 02:40:55 +0500

    Initial Commit

---
+++ google-places-api-by-mzs.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @package GooglePlacesApi
+ */
+
+ /*
+ Plugin Name: Google Places Api Plugin
+ Plugin URI: http://mzubairsaleem.com/plugins/google-places-api-by-mzs
+ Description: Read Data From Google Places API And Save In Database. Then Displays In Table With The Use Of Datatables.
+ Version: 1.0.0
+ Author: Muhammad Zubair Saleem
+ Author URI: http://mzubairsaleem.com
+ License: GNU General Public License v2 or later
+ License URI: http://www.gnu.org/licenses/gpl-2.0.html
+ Text Domain: google-places-api-by-mzs
+
+
+ Google Places Api Plugin is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 2 of the License, or any later version.
+
+ Google Places Api Plugin is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License along with Google Places Api Plugin. If not, Get a copy of the GNU General Public License in <http://www.gnu.org/licenses/>.
+*/
