b49ea573c7805761ead3f8af09608fcc067c10eb
Muhammad Zubair Saleem <mzubairsaleem@outlook.com>
2018-04-14 02:44:31 +0500

    Indent Fixes

--- google-places-api-by-mzs.php
+++ google-places-api-by-mzs.php
@@ -16,8 +16,6 @@
 
 
  Google Places Api Plugin is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 2 of the License, or any later version.
-
  Google Places Api Plugin is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
-
  You should have received a copy of the GNU General Public License along with Google Places Api Plugin. If not, Get a copy of the GNU General Public License in <http://www.gnu.org/licenses/>.
 */
